fa578d3f3fbdf6be603fd42556676889e80c26c0
ChristianFechner <kaputtnick01@yahoo.de>
2025-03-03 16:01:38 +0100

    4.3.25

---
+++ main.dart
@@ -0,0 +1,13 @@
+void main() {
+  bool ParentalConsent = true;
+  int age = 20;
+  
+  }
+
+
+ bool movieAgeRating = age >= 18;
+ if(movieAgeRating < && ParentalConsent){
+  print (` Film schauen erlaubt `);
+ } else { (age < 18 );
+ print ( `darf den Film nicht schauen `);
+ }
