8f09e187a44b86dcf33a99f1003e747d9f421532
ChristianFechner <kaputtnick01@yahoo.de>
2025-03-04 15:54:08 +0100

    5.3

--- 5.3.25/main.dart
+++ 5.3.25/main.dart
@@ -1,5 +1,5 @@
 void main() {
-  List<Map<String, double?>>weatherData =[
+  List<Map< bool, double?>>weatherData =[
   {´ temp ´: 5.3, ´ rain ´: 0.9, `wind `: null},
   {´ temp ´: 4.5, ´ rain ´: null, `wind `: 16.8},
   {´ temp ´: null, ´ rain ´: 3.8, `wind `: null},
@@ -12,6 +12,7 @@ void main() {
 
   double? avgTemp = (temps[0] + temps[1] + temps[2]) / 3;
 
-  print( ´Durchschnittstemperatur: $avgTemp`);
+  print ( ´Durchschnittstemperatur: $avgTemp`);
+
 
 }
